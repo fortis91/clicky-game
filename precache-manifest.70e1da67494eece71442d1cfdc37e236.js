00f9aa09124c17167fa1514e4ff3341705f5a200
Nigel <neastern101@yahoo.com>
2019-07-26 13:17:45 -0500

    Updates

---
+++ precache-manifest.70e1da67494eece71442d1cfdc37e236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c89f96d8c1cea785ffef44f883937a3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "c21de13192df361792e1",
+    "url": "/clicky-game/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c21de13192df361792e1",
+    "url": "/clicky-game/static/js/main.a4176642.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
