283132f766fca894376a2ff8fdd37fb07e139707
Nigel <neastern101@yahoo.com>
2019-07-26 13:23:34 -0500

    remove boiling plate code

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ class App extends Component {
   };
 
   removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
     const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
     this.setState({ friends });
   };
 
